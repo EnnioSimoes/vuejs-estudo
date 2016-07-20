fc9a41d9a8f55eeff5599615e22d4bf6917d4d4e
Ennio <ennio.simoes@outlook.com>
2016-07-19 23:13:43 -0300

    first commit

---
+++ course/js/app.js
@@ -0,0 +1,13 @@
+var hello = new Vue({
+    el: '#hello',
+    data: {
+        msg: 'Hello World!',
+        peoples: [
+            {name: "Ennio"},
+            {name: "Greicy"},
+            {name: "Eliane"},
+            {name: "Nilson"},
+            {name: "Chico"}
+        ]
+    }
+});
