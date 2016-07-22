c67731397e8e3a986c2ae870cce6659ef09877a1
Ennio Simões <ennio.simoes@outlook.com>
2016-07-22 13:59:15 -0300

    add filterBy

--- course/js/app.js
+++ course/js/app.js
@@ -1,51 +1,16 @@
-var hello = new Vue({
-    el: '#hello',
+var app = new Vue({
+    el: '#app',
     data: {
-        msg: 'Hello World!',
-        peoples: [
-            {name: "Ennio"},
-            {name: "Greicy"},
-            {name: "Eliane"},
-            {name: "Nilson"},
-            {name: "Chico"}
-        ],
-        newElement: '',
-        elements: [],
-        objectA: {
-            'font-size': '30px'
-        },
-        objectB: {
-            color: 'red'
-        },
-        myListForm: [],
-        myForm: {
-            name: '',
-            email: ''
-        }
+        books: [],
+        MySearch: ''
     },
     methods: {
-        addElement: function() {
-            var title = this.newElement.trim();
-            if (title) {
-                this.elements.push({title:title});
-                this.newElement = "";
-            }
 
-        },
-        removeElement: function(e, index) {
-            e.preventDefault();
-            this.elements.splice(index, 1);
-        },
-        myClick: function() {
-            alert('Click');
-        },
-        addForm: function() {
-            this.myListForm.push({
-                name: this.myForm.name,
-                email: this.myForm.email
-            });
-            this.myForm.name = '';
-            this.myForm.email = '';
-        }
+    },
+    ready: function() {
+        var self = this;
+        self.$http.get('dataServer.json').then(function(response) {
+            self.books = response.data;
+        });
     }
 });
---
+++ course/js/app.testes.js
@@ -0,0 +1,56 @@
+var hello = new Vue({
+    el: '#hello',
+    data: {
+        msg: 'Hello World!',
+        peoples: [
+            {name: "Ennio"},
+            {name: "Greicy"},
+            {name: "Eliane"},
+            {name: "Nilson"},
+            {name: "Chico"}
+        ],
+        newElement: '',
+        elements: [],
+        objectA: {
+            'font-size': '30px'
+        },
+        objectB: {
+            color: 'red'
+        },
+        myListForm: [],
+        myForm: {
+            name: '',
+            email: ''
+        }
+    },
+    methods: {
+        addElement: function() {
+            var title = this.newElement.trim();
+            if (title) {
+                this.elements.push({title:title});
+                this.newElement = "";
+            }
+        },
+        removeElement: function(e, index) {
+            e.preventDefault();
+            this.elements.splice(index, 1);
+        },
+        myClick: function() {
+            alert('Click');
+        },
+        addForm: function() {
+            console.log(this.myListForm);
+            this.myListForm.push({
+                name: this.myForm.name,
+                email: this.myForm.email
+            });
+            this.myForm.name = '';
+            this.myForm.email = '';
+        },
+        removeForm: function(e, index) {
+            e.preventDefault();
+            console.log(index);
+            this.myListForm.splice(index, 1);
+        }
+    }
+});
